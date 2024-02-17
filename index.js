60a35ae39a13a3f89b7329fb85ad4ae2ac8bb791
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-17 15:24:06 -0700

    almost...

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { Simulation, DynamicBody, Vec2 } from './gravitypong.js';
 const s = Simulation.getInstance();
 s.addBody(new DynamicBody(1000, 10));
 s.addBody(new DynamicBody(10, 4, new Vec2(100, 0), new Vec2(10, 5)));
-s.run();
+console.log(s);
+// s.run();
--- index.ts
+++ index.ts
@@ -6,4 +6,5 @@ const s = Simulation.getInstance();
 s.addBody(new DynamicBody(1000, 10))
 s.addBody(new DynamicBody(10, 4, new Vec2(100, 0), new Vec2(10, 5)));
 
-s.run();
+console.log(s);
+// s.run();
