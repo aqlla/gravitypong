c8fb513767287fbcba493180ea7c157e257a57a5
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-18 16:17:14 -0700

    ok, almost got it again...

--- dist/vector_generic.js
+++ dist/vector_generic.js
@@ -27,7 +27,7 @@ export class Vec2GenericBase {
             return this;
         }
         else {
-            return new (self.constructor(...components));
+            return new window[self.constructor.name](...components);
         }
     }
     eq(other) {
--- src/vector_generic.ts
+++ src/vector_generic.ts
@@ -57,7 +57,7 @@ export class Vec2GenericBase<TNum = number> implements IOrderedPair<TNum> {
             [this.x, this.y] = components;
             return this;
         } else {
-            return new (self.constructor(...components));
+            return new (<any>window)[self.constructor.name](...components);
         }
     }
 
