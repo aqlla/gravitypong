00ed97b9a91b9b786135020de5aa46d265a9219f
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 15:06:34 -0700

    tweak acc clamp

--- dist/util.js
+++ dist/util.js
@@ -1,6 +1,6 @@
 export function maxOf(...ns) {
     return Math.max(...ns);
 }
-export function clamp(n, min = Number.EPSILON * 100) {
+export function clamp(n, min = Number.EPSILON * 10) {
     return maxOf(n, min);
 }
--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,6 @@ export function maxOf(...ns: number[]): number {
     return Math.max(...ns);
 }
 
-export function clamp(n: number, min: number = Number.EPSILON * 100): number {
+export function clamp(n: number, min: number = Number.EPSILON * 10): number {
     return maxOf(n, min);
 }
