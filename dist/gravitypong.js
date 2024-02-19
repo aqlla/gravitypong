3ccdfae3ebee19f5c5118c082af23f8affcba37e
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 14:52:22 -0700

    tweak body radius scale

--- dist/gravitypong.js
+++ dist/gravitypong.js
@@ -44,7 +44,7 @@ function updateAcceleration2(bodies) {
             const distanceSquared = r.magnitudeSquared;
             const distance = Math.sqrt(distanceSquared) * DISTANCE_SCALE;
             // console.log(distance)
-            if (distance < (b1.r + b2.r) * 100) {
+            if (distance < (b1.r + b2.r) * 500) {
                 // collide
                 if (!bodysInCollisionsList(collisions, b1, b2)) {
                     b1.acc = Vec2.zero;
