08a9e7631575da2516ba13976d4f63ad24459f47
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 21:08:03 -0700

    fix static

--- src/gravitypong.ts
+++ src/gravitypong.ts
@@ -242,7 +242,7 @@ export class Simulation extends GameLoopBase {
 
                 const body = new DynamicBody({ 
                     pos: pos,
-                    vel: new Vec2(pos.y / distanceFromOrigin * 80000, -pos.x / distanceFromOrigin * 80000),
+                    vel: new Vec2(pos.y / distanceFromOrigin * 150000, -pos.x / distanceFromOrigin * 150000),
                 })
 
                 Simulation.instance.addBody(body);
