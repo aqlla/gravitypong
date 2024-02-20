a0e91882e7578cf79fd52159add02276ad40a7c5
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 22:32:53 -0700

    tweak init pos

--- src/gravitypong.ts
+++ src/gravitypong.ts
@@ -258,11 +258,11 @@ export class Simulation extends GameLoopBase {
 
     // 100.000.000x 
     public static get max_pos(): number {
-        return 500;
+        return 1000;
     }
 
     public static get min_pos(): number {
-        return -500;
+        return -1000;
     }
 
     public static getRandomPos(max = Simulation.max_pos, min = Simulation.min_pos): Vec2 {
