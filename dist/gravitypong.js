54e53a4755cbdcef02f9cb6f6202df80e8bbdce9
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-20 00:16:13 -0700

    add fps

--- dist/gravitypong.js
+++ dist/gravitypong.js
@@ -171,7 +171,7 @@ export class Simulation extends GameLoopBase {
                 const distanceFromOrigin = pos.magnitudeSquared;
                 const body = new DynamicBody({
                     pos: pos,
-                    vel: new Vec2(pos.y / distanceFromOrigin * 700000, -pos.x / distanceFromOrigin * 700000),
+                    vel: new Vec2(pos.y / distanceFromOrigin * 600000, -pos.x / distanceFromOrigin * 600000),
                 });
                 Simulation.instance.addBody(body);
             }
