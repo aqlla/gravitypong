b7b5bb406a0fc063ccdfc173e81b4fd09d067f37
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 22:00:57 -0700

    tweak collision thresh

--- src/gravitypong.ts
+++ src/gravitypong.ts
@@ -59,7 +59,7 @@ function updateAcceleration2(bodies: BodyList) {
             const distance = Math.sqrt(distanceSquared) * DISTANCE_SCALE;
             // console.log(distance)
             
-            if (distance < (b1.r + b2.r) * 800 && !bodysInCollisionsList(collisions, b1, b2)) {
+            if (distance < (b1.r + b2.r) * 500 && !bodysInCollisionsList(collisions, b1, b2)) {
                 // collide
                 b1.acc = Vec2.zero;
                 b2.acc = Vec2.zero;
