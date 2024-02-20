3124bef87335eb2f1ea79761c6cd47b334f454d5
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 19:57:38 -0700

    tweak init velocity

--- src/gravitypong.ts
+++ src/gravitypong.ts
@@ -225,7 +225,7 @@ export class Simulation extends GameLoopBase {
 
             // Sun
             Simulation.instance.addBody(new DynamicBody({
-                m: DynamicBody.max_mass * 100,
+                m: DynamicBody.max_mass * 1000,
                 pos: new Vec2(0, 0),
             }));
 
