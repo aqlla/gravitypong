489489c20fdb6bf7eca909db93fba0eeed713ef1
Aquilla Sherrock <acsherrock@gmail.com>
2024-02-19 22:24:06 -0700

    tweak init velocity

--- src/gravitypong.ts
+++ src/gravitypong.ts
@@ -242,7 +242,7 @@ export class Simulation extends GameLoopBase {
 
                 const body = new DynamicBody({ 
                     pos: pos,
-                    vel: new Vec2(pos.y / distanceFromOrigin * 600000, -pos.x / distanceFromOrigin * 600000),
+                    vel: new Vec2(pos.y / distanceFromOrigin * 500000, -pos.x / distanceFromOrigin * 500000),
                 })
 
                 Simulation.instance.addBody(body);
